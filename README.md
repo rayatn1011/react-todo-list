52a6bed3c7e3699e4d3ba8e628f95454ec6b1d49
ChunRen Zhang <rayatn1011@gmail.com>
2024-09-14 02:27:59 +0800

    feat: init app structure

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # react-todo-list
+
 react-todo-list
+
+```env
+VITE_API_BASE_URL=
+VITE_API_PROXY_BASE_URL=
+```
---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
+// https://vitejs.dev/config/
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': '/src',
+      },
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_PROXY_BASE_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
+    },
+  };
+});
