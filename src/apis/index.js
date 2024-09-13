ce4c346da7ebdee468739a17e9ad0acb3f6e0f7a
ChunRen Zhang <rayatn1011@gmail.com>
2024-09-14 02:27:59 +0800

    feat: add users api

---
+++ src/apis/index.js
@@ -0,0 +1,3 @@
+export * from './use-sign-in';
+export * from './use-sign-out';
+export * from './use-sign-up';
---
+++ src/apis/use-sign-in.js
@@ -0,0 +1,20 @@
+import { useFetch } from '@/fetcher';
+
+/**
+ * 使用者登入
+ *
+ * @param {object} data
+ * @param {object} data.user
+ * @param {string} data.user.email
+ * @param {string} data.user.password
+ */
+export const useSignIn = (data, ...rest) => {
+  const apiUrl = '/users/sign_in';
+  const fetchObj = useFetch(apiUrl, {
+    method: 'POST',
+    data,
+    ...rest,
+  });
+
+  return fetchObj;
+};
