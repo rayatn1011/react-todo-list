0df82587c45c669a55e3621f80f95a0f38192e81
ChunRen Zhang <rayatn1011@gmail.com>
2024-09-21 03:36:56 +0800

    feat: add error-boundary page

---
+++ src/features/routes/error-boundary/error-boundary.jsx
@@ -0,0 +1,24 @@
+import { useNavigate, useRouteError } from 'react-router-dom';
+import { Button } from '@/features/ui';
+import './style.scss';
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+  const navigate = useNavigate();
+  const stringifyError = JSON.stringify(
+    error,
+    Object.getOwnPropertyNames(error),
+  );
+
+  const handleClick = () => {
+    navigate('/');
+  };
+
+  return (
+    <article className="error-boundary">
+      <h1 className="error-boundary__title">哇...哪裡出了問題</h1>
+      <p className="error-boundary__content">{stringifyError}</p>
+      <Button onClick={handleClick}>回首頁</Button>
+    </article>
+  );
+}
