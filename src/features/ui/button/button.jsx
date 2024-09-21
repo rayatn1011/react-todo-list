ab7a4a4f522e545a55e608fdec2671f9014071d5
ChunRen Zhang <rayatn1011@gmail.com>
2024-09-22 02:14:51 +0800

    feat: add button jsdoc and optimize button style

--- src/features/ui/button/button.jsx
+++ src/features/ui/button/button.jsx
@@ -1,5 +1,11 @@
 import './style.scss';
 
+/**
+ * @param {React.DetailedHTMLProps<
+ *   React.ButtonHTMLAttributes<HTMLButtonElement>,
+ *   HTMLButtonElement
+ * >}
+ */
 export const Button = ({ children, className = '', ...rest }) => {
   return (
     <button className={`ui-button ${className}`} {...rest}>
